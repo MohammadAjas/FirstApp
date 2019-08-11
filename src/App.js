5df1c24eddf5bea8fc5348cd04407bac7d655f0f
MohammadAjas <getajas4u@gmail.com>
2019-08-11 21:31:39 +0530

    Firt App

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Ajas',
+      age: 26
+    }
+    // this.fullName = this.fullName.bind(this)
+  }
+  fullName(payload) {
+    this.setState({
+      name: payload
+    })
+  }
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='name'>{this.state.name}</h1>
+        <span>Name:</span>
+        <button onClick={() => this.fullName('New Name')}>Show full name</button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
